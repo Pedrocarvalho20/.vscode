7c827c450891c1057f959676f6dc81060cd6c756
Pedro Carvalho <pedrocarvalho3232@hotmail.com>
2024-04-23 13:56:53 -0300

    Tempo360

--- scripts.js
+++ scripts.js
@@ -1,38 +1,53 @@
-let chave = '6d5b190d07bf77b89893d1c34f035cec'
+let chave = '6d5b190d07bf77b89893d1c34f035cec';
 
-function colocarNaTela(dados){
-    console.log(dados)
+function colocarNaTela(dados) {
+    console.log(dados);
     
-    document.querySelector ('.cidade').innerHTML = 'Tempo em ' + dados.name
-    document.querySelector ('.graus').innerHTML = Math.floor(dados.main.temp) + '°C'
-    document.querySelector ('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon +".png"
-    document.querySelector ('.umidade').innerHTML = dados.main.humidity + '%'
+    if (dados.name) {
+        document.querySelector('.cidade').innerHTML = 'Tempo em ' + dados.name;
+        document.querySelector('.graus').innerHTML = Math.floor(dados.main.temp) + '°C';
+        document.querySelector('.icone').src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
+        document.querySelector('.umidade').innerHTML = dados.main.humidity + '%';
+    } else {
+        alert('Não foi possível obter os dados de localização. Tente novamente mais tarde.');
+    }
 }
 
-
+async function buscarCidade(cidade) {
+    console.log(cidade);
     
-    async function buscarCidade(cidade) {
-        
-        console.log(cidade)
-        
-        let dados = await fetch(
-            'https://api.openweathermap.org/data/2.5/weather?q=' +
-            cidade +
-            '&appid=6d5b190d07bf77b89893d1c34f035cec&units=metric',
-        ).then((resposta) => resposta.json())
-    //await = espere
-    //fetch -> ferramenta js para acessar servidor
-    //then -> então
-    //json -> js object notation  (o formanto que o js entende)
+    let dados = await fetch(
+        'https://api.openweathermap.org/data/2.5/weather?q=' +
+        cidade +
+        '&appid=' + '6d5b190d07bf77b89893d1c34f035cec' + '&units=metric'
+    ).then((resposta) => resposta.json());
 
-    colocarNaTela(dados)
+    colocarNaTela(dados);
+}
+
+async function buscarLocalizacao() {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(async function(position) {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+
+            let dados = await fetch(
+                'https://api.openweathermap.org/data/2.5/weather?lat=' +
+                latitude + '&lon=' + longitude +
+                '&appid=' + '6d5b190d07bf77b89893d1c34f035cec' + '&units=metric'
+            ).then((resposta) => resposta.json());
+
+            colocarNaTela(dados);
+        });
+    } else {
+        alert('Geolocalização não é suportada pelo seu navegador.');
+    }
 }
 
 function cliqueiNoBotao() {
-    let cidade = document.querySelector('.input-cidade').value
+    let cidade = document.querySelector('.input-cidade').value;
 
-
-    buscarCidade(cidade)
+    buscarCidade(cidade);
 }
 
 /*clicar no botão
